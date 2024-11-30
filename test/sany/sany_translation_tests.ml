5c329cdc6174087bb22fbcaecfbf94c350fc5ff3
Andrew Helwer <2n8rn1w1f@mozmail.com>
2024-11-30 10:30:58 -0500

    Explored TLAPM semantic parse tree output
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- test/sany/sany_translation_tests.ml
+++ test/sany/sany_translation_tests.ml
@@ -14,8 +14,11 @@ let () =
     let text = ic |> In_channel.input_all in
     close_in ic;
     match modctx_of_string ~content:text ~filename:"AddTwo.tla" ~loader_paths:[] ~prefer_stdlib:true with
-    | Ok _ -> print_endline "Success!"
-    | Error _ -> print_endline "Failure."
+    | Ok (ctx, parse_tree) -> print_endline "Success!"
+    | Error (location, err_msg) ->
+      let err_loc = location |> Option.value ~default:"unknown location" in
+      print_endline (Printf.sprintf "Error at %s:" err_loc);
+      print_endline err_msg;
   with e ->
     close_in_noerr ic;
     raise e
